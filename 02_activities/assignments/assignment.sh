33f8b8770d476a819f4f061cec57d23703b1fc37
Atsu Vovor <atsu.vovor@bell.net>
2026-02-08 00:38:13 -0500

    Question8 answered

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -54,7 +54,7 @@ rm -f data/raw/*ipaddr*
 rm -f data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-#find data/processed -type f > data/inventory.txt
+find data/processed -type f > data/inventory.txt
 
 
 ###########################################
